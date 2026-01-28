680a942d4bd5eadf806ba5ddfd3a4efb4222e934
AnaniyaYosef <yosefananiya@gmail.com>
2026-01-28 16:05:50 +0300

    small change

--- utils/DataBaseFrame.py
+++ utils/DataBaseFrame.py
@@ -13,18 +13,3 @@ cursor = db.cursor()
 
 def CreateDB():
     Add_DB.CreateEmpTable()
-
-
-
-def PermantDelete_DB_Table():
-    cursor.execute("SET FOREIGN_KEY_CHECKS = 0")
-    cursor.execute("SHOW TABLES")
-    tables = cursor.fetchall()
-
-    for (table_name,) in tables:
-        cursor.execute(f"DROP TABLE IF EXISTS `{table_name}`")
-
-    cursor.execute("SET FOREIGN_KEY_CHECKS = 1")
-    db.commit()
-    print("All tables deleted successfully.")
-
